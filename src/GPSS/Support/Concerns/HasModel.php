0d0c4ea4c2128f7563c10743ff71ef86baa7728c
rugleb <rugleb@gmail.com>
2018-05-14 00:45:26 +0300

    create HasModel trait

---
+++ src/GPSS/Support/Concerns/HasModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace GPSS\Support\Concerns;
+
+use GPSS\Foundation\Model;
+
+/**
+ * The HasModel trait.
+ *
+ * @package src\GPSS\Support\Concerns
+ */
+trait HasModel
+{
+    /**
+     * The Model instance.
+     *
+     * @var Model
+     */
+    protected $model;
+    
+    /**
+     * Get Model instance.
+     *
+     * @return Model|null
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set Model instance.
+     *
+     * @param Model $model    Model instance.
+     * @return static
+     */
+    public function setModel(Model &$model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Determines if the model instance is installed.
+     *
+     * @return bool
+     */
+    public function hasModel(): bool
+    {
+        return $this->getModel() instanceof Model;
+    }
+
+}
